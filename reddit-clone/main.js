cd1899483ab192c03ad65bf4b4f0c7e90ccc6f40
Patrick <pcnicklaus@gmail.com>
2015-09-17 10:21:16 -0600

    updated reddit clone to include working comment section

--- reddit-clone/main.js
+++ reddit-clone/main.js
@@ -1,84 +1,63 @@
-        var myApp = angular.module('myApp',['angularMoment']);
-        myApp.controller('redditController', ['$scope', function($scope) {
+var myApp = angular.module('myApp',['angularMoment']);
+myApp.controller('redditController', ['$scope', function($scope) {
 
-            //sort functionality
-            $scope.options = ['votes', 'date', 'title'];
+    //sort functionality
+    $scope.options = ['votes', 'date', 'title'];
 
-            // hardcoded info
-            $scope.posts = [
-                  {
-                    title: "One title",
-                    author: "some guy",
-                    description: "some fancy words about something",
-                    image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
-                    comments: [],
-                    votes: 0
-                  },
-                  {
-                    title: "Two titles",
-                    author: "a different guy",
-                    description: "some other fancy words about something",
-                    image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
-                    comments: [],
-                    votes: 0
-                  },
-                  {
-                    title: "Three titles, hah, hah, hah, hah",
-                    author: "some other, other guy",
-                    description: "some fancy words about something",
-                    image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
-                    comments: [],
-                    votes: 0
-
-                  }
-                ];
-
-            // voting functionality - not working
-            $scope.upVote = function (place) {
-              place += 1;
-            };
-            $scope.downVote = function () {
-              $scope.votes --;
-            };
-
-            //add post stuffs
-            $scope.addPost = function () {
-              $scope.posts.push({
-                'title': $scope.title,
-                'author': $scope.author,
-                'description': $scope.description,
-                'image': $scope.image,
-                'comments': [],
-                'date': Date.now()
-              });
-              $scope.title = '';
-              $scope.author = '';
-              $scope.description = '';
-              $scope.image = '';
-
-            };
-
-            // $scope.addComment = function () {
-            //   $scope.post.comments.push({
-            //     'title': $scope.commentTitle,
-            //     'author': $scope.commentAuthor,
-            //     'description': $scope.commentDescription,
-            //     'date': Date.now()
-            //   });
-            //   console.log(comments);
-            //   $scope.title = '';
-            //   $scope.author = '';
-            //   $scope.description = '';
-            //   $scope.image = '';
-            // };
-
-           $scope.addComment = function(){
-               this.post.comments.push({author:this.commentAuthor, comment:this.commentDescription});
-              $scope.commentAuthor = '';
-              $scope.commentDescription = '';
-             };
+    // hardcoded info
+    $scope.posts = [
+          {
+            title: "One title",
+            author: "some guy",
+            description: "some fancy words about something",
+            image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+            comments: [],
+            votes: 0
+          },
+          {
+            title: "Two titles",
+            author: "a different guy",
+            description: "some other fancy words about something",
+            image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+            comments: [],
+            votes: 0
+          },
+          {
+            title: "Three titles, hah, hah, hah, hah",
+            author: "some other, other guy",
+            description: "some fancy words about something",
+            image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+            comments: [],
+            votes: 0
+          }
+        ];
 
 
-        }]);
+    //add post stuffs
+    $scope.addPost = function () {
+      $scope.posts.push({
+        'title': $scope.title,
+        'author': $scope.author,
+        'description': $scope.description,
+        'image': $scope.image,
+        'comments': [],
+        'date': Date.now()
+      });
+      $scope.title = '';
+      $scope.author = '';
+      $scope.description = '';
+      $scope.image = '';
 
-//show and hide for the vote thing...
+    };
+
+
+   $scope.addComment = function(){
+       this.post.comments.push({author:this.commentAuthor, comment:this.commentDescription});
+      $scope.commentAuthor = '';
+      $scope.commentDescription = '';
+      console.log($scope.posts);
+     };
+
+
+}]);
+
