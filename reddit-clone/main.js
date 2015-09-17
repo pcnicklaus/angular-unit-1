3c9e3ee58378f6322805e9f3355fa86f4e7c409d
Patrick <pcnicklaus@gmail.com>
2015-09-17 13:12:58 -0600

    reworking the post submissions to look better

--- reddit-clone/main.js
+++ reddit-clone/main.js
@@ -10,15 +10,16 @@ myApp.controller('redditController', ['$scope', function($scope) {
             title: "One title",
             author: "some guy",
             description: "some fancy words about something",
-            image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/63/Nice-seafront.jpg",
             comments: [],
+
             votes: 0
           },
           {
             title: "Two titles",
             author: "a different guy",
             description: "some other fancy words about something",
-            image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Nice-night-view-with-blurred-cars_1200x900.jpg",
             comments: [],
             votes: 0
           },
@@ -26,7 +27,7 @@ myApp.controller('redditController', ['$scope', function($scope) {
             title: "Three titles, hah, hah, hah, hah",
             author: "some other, other guy",
             description: "some fancy words about something",
-            image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+            image: "https://upload.wikimedia.org/wikipedia/commons/0/02/Nice_view_(3201807289).jpg",
             comments: [],
             votes: 0
           }
@@ -52,10 +53,11 @@ myApp.controller('redditController', ['$scope', function($scope) {
 
 
    $scope.addComment = function(){
+    console.log(this)
        this.post.comments.push({author:this.commentAuthor, comment:this.commentDescription});
       $scope.commentAuthor = '';
       $scope.commentDescription = '';
-      console.log($scope.posts);
+
      };
 
 
